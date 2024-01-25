d0b1b165b0fbf29ee6d9dc2cdd489e9b60e340d0
Franco Riolfo <148019172+RiolfoF4@users.noreply.github.com>
2024-01-25 20:35:37 -0300

    Comentarios borrados

--- unitmenu.pas
+++ unitmenu.pas
@@ -15,19 +15,13 @@ procedure MostrarMenu;
     ArbolApYNom: TPuntApYNom;
     ArbolDNI: TPuntDNI;
     Op: String[2];
-    {PosX, PosY: Word;}
   begin
     Inicializar(ArchCon, ArchInf, ArbolApYNom, ArbolDNI);
     repeat
       ClrScr;
-      {PosX := EsqX; PosY := EsqY;}
-      {GotoXY(PosX, PosY);}
       WriteLn('[1] Ingresar Apellido y Nombre.');
-      {PosY += 1; GotoXY(PosX, PosY);}
       WriteLn('[2] Ingresar DNI.');
-      {PosY += 1; GotoXY(PosX, PosY);}
       WriteLn('[0] Salir.');
-      {PosY += 2; GotoXY(PosX, PosY);}
       WriteLn;
       Write('Opción: ');
       ReadLn(Op);
