387ebbe97d9a6c9f97c6ffdbce010615761637f3
Franco Riolfo <148019172+RiolfoF4@users.noreply.github.com>
2024-01-31 21:15:56 -0300

    Update unitlista.pas

--- unitlista.pas
+++ unitlista.pas
@@ -7,34 +7,34 @@ const
 
 type
   TDatoLista = String;
-  TLista = record
+  TListaTiposInf = record
     Elem: array[1..N] of TDatoLista;
     Tam: Word;
   end;
 
-procedure CrearLista(var L: TLista);
-procedure Agregar(var L: TLista; x: TDatoLista);
-procedure Eliminar(var L: TLista; Pos: Word; var x: TDatoLista);
-procedure DesplazarLista(var L: TLista; Pos: Word);
-procedure Recuperar(var L: TLista; Pos: Word; var x: TDatoLista);
-procedure Modificar(var L: TLista; Pos: Word; x: TDatoLista);
-function TamanioLista(var L: TLista): Word;
-function ListaLlena(var L: TLista): Boolean;
-function ListaVacia(var L: TLista): Boolean;
+procedure CrearLista(var L: TListaTiposInf);
+procedure Agregar(var L: TListaTiposInf; x: TDatoLista);
+procedure Eliminar(var L: TListaTiposInf; Pos: Word; var x: TDatoLista);
+procedure DesplazarLista(var L: TListaTiposInf; Pos: Word);
+procedure Recuperar(var L: TListaTiposInf; Pos: Word; var x: TDatoLista);
+procedure Modificar(var L: TListaTiposInf; Pos: Word; x: TDatoLista);
+function TamanioLista(var L: TListaTiposInf): Word;
+function ListaLlena(var L: TListaTiposInf): Boolean;
+function ListaVacia(var L: TListaTiposInf): Boolean;
 
 implementation
-procedure CrearLista(var L: TLista);
+procedure CrearLista(var L: TListaTiposInf);
   begin
     L.Tam := 0;
   end;
 
-procedure Agregar(var L: TLista; x: TDatoLista);
+procedure Agregar(var L: TListaTiposInf; x: TDatoLista);
   begin
     Inc(L.Tam);
     L.Elem[L.Tam] := x;
   end;
 
-procedure Eliminar(var L: TLista; Pos: Word; var x: TDatoLista);
+procedure Eliminar(var L: TListaTiposInf; Pos: Word; var x: TDatoLista);
   begin
     x := L.Elem[Pos];
     Dec(L.Tam);
@@ -42,7 +42,7 @@ procedure Eliminar(var L: TLista; Pos: Word; var x: TDatoLista);
       DesplazarLista(L, Pos);
   end;
 
-procedure DesplazarLista(var L: TLista; Pos: Word);
+procedure DesplazarLista(var L: TListaTiposInf; Pos: Word);
   var
     i: 1..N;
   begin
@@ -50,27 +50,27 @@ procedure DesplazarLista(var L: TLista; Pos: Word);
       L.Elem[i] := L.Elem[i+1];
   end;
 
-procedure Recuperar(var L: TLista; Pos: Word; var x: TDatoLista);
+procedure Recuperar(var L: TListaTiposInf; Pos: Word; var x: TDatoLista);
   begin
     x := L.Elem[Pos];
   end;
 
-procedure Modificar(var L: TLista; Pos: Word; x: TDatoLista);
+procedure Modificar(var L: TListaTiposInf; Pos: Word; x: TDatoLista);
   begin
     L.Elem[Pos] := x;
   end;
 
-function TamanioLista(var L: TLista): Word;
+function TamanioLista(var L: TListaTiposInf): Word;
   begin
     TamanioLista := L.Tam;
   end;
 
-function ListaLlena(var L: TLista): Boolean;
+function ListaLlena(var L: TListaTiposInf): Boolean;
   begin
     ListaLlena := (L.Tam = N);
   end;
 
-function ListaVacia(var L: TLista): Boolean;
+function ListaVacia(var L: TListaTiposInf): Boolean;
   begin
     ListaVacia := (L.Tam = 0);
   end;
